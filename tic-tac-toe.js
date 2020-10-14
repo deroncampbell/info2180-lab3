585ea064efa9f5c6297b960ae088d03156df29dc
Deron Campbell <deroncampbell88@gmail.com>
2020-10-13 21:42:27 -0500

    Completed exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,14 @@
+const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+];
+
 let playMessage;
 let playerTurn;
 
@@ -32,5 +43,28 @@ function clickHandler() {
     this.innerText = playerTurn;
     this.classList.add(playerTurn);
 
+    if (getWinner()) {
+        status.innerText = `Congratulations! ${playerTurn} is the Winner!`;
+        status.classList.add('you-won');
+        document
+            .querySelectorAll('#board > div')
+            .forEach((el) => el.removeEventListener('click', clickHandler, { once: true }));
+    }
+
     playerTurn = playerTurn === 'X' ? 'O' : 'X';
+}
+
+function getWinner() {
+    els = document.querySelectorAll('#board > div');
+    for (let win of wins) {
+        let a = els[win[0]].innerText;
+        let b = els[win[1]].innerText;
+        let c = els[win[2]].innerText;
+
+        if (a && a == b && b == c) {
+            return a;
+        }
+    }
+
+    return null;
 }
